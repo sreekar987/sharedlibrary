2c1f0db7984873fa59355dc94255817199078d8d
sreekar987 <79581962+sreekar987@users.noreply.github.com>
2022-02-16 21:16:16 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:])
 {
- git (url: "${config:Giturl}",
- branch: "${config:Gitbranch}" ,
+ git (url: "${config.Giturl}",
+ branch: "${config.Gitbranch}" ,
  credentialsId: "Git_hub_credentails" )
 }
